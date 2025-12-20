e2b982c4b8c3ff838353985feb132ece874c1438
SERENA FERRARO <serenaferraro21@gmail.com>
2025-12-20 14:47:43 +0000

    Created categories.json, test_categorization and added categorize_transaction to main

--- src/main.py
+++ src/main.py
@@ -1,25 +1,41 @@
+#!/usr/bin/env python3
+import json
 import pandas as pd
 import os
 import csv
 
-bank_statments_path = os.getenv("BANK_STATMENTS_PATH")
+
+
+config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'categories.json')
+with open(config_path) as f:
+    categories_dic = json.loads(f.read())
+
+def categorize_transaction(description, categories_dic):
+    # Function to categorize a transaction based on its description
+    description_lower = description.lower()
+    for category, keywords in categories_dic.items():
+        for keyword in keywords:
+            if keyword in description_lower:  
+                return category
+    return "Uncategorized"
 
 def process_bank_statements(path):
     # Function to process bank statements  
     files_list = os.listdir(path) 
     csv_files = [file for file in files_list if file.endswith('.csv')]
     for file in csv_files:
-        print(f"Processing file: {file}")
+        # print(f"Processing file: {file}")
         file_df = pd.read_csv(os.path.join(path, file), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        file_df.info()
+        # file_df.info()
         
 
+if __name__ == "__main__":
+ bank_statements_path = os.getenv("BANK_STATEMENTS_PATH")
 
-if bank_statments_path:
-    print(f"Loading bank statements from: {bank_statments_path}")
-    process_bank_statements(bank_statments_path)
-    
-elif not bank_statments_path:
+ if bank_statements_path:
+    # print(f"Loading bank statements from: {bank_statements_path}")
+    process_bank_statements(bank_statements_path)
+ elif not bank_statements_path:
     raise ValueError("BANK_STATMENTS_PATH environment variable is not set.")
 
 
---
+++ tests/test_categorization.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import json
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))   
+from main import categorize_transaction
+
+import json
+with open('config/categories.json') as f:
+    categories = json.load(f)
+
+def test_aldi():
+    result = categorize_transaction("ALDI Supermarket", categories)
+    assert result == "supermarket"
+
+def test_starbucks():
+    result = categorize_transaction("Starbucks Coffee", categories)
+    assert result == "eating_out"
+
+def test_unknown():
+    result = categorize_transaction("Negozio Sconosciuto", categories)
+    assert result == "Uncategorized"
+
+
+if __name__ == "__main__":
+    test_aldi()
+    test_starbucks()
+    test_unknown()
+    print("All tests passed!")
